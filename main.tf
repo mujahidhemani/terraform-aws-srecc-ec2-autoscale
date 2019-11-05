95942458ab8d1828df4ddad05e9f09763d734865
Mujahid Hemani <9062439+mujahidhemani@users.noreply.github.com>
2019-11-05 02:48:52 +0000

    terraform fmt

--- main.tf
+++ main.tf
@@ -54,17 +54,17 @@ resource "aws_security_group" "outyet-sg" {
   vpc_id      = "${var.vpc_id}"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    self = true
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+    self      = true
   }
 
   egress {
     from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
-    self = true
+    to_port   = 8080
+    protocol  = "tcp"
+    self      = true
   }
 
   lifecycle {
